43fbd73ef1230fcfc142cdb5c4248d81091953d1
rsauermann <raphaelsauermann@yahoo.de>
2020-07-05 09:46:12 +0200

    added navbar, based on wireframe

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="de" dir="ltr">
   <head>
-    <meta charset="UTF-8">
+    <!-- <meta charset="UTF-8">
     <title></title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -10,17 +10,43 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 
+    <link rel="stylesheet" href="res/css/custom.css">
+
+    <script type="text/javascript" src="res/js/PictureCloud.js"></script>
+ -->
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
     <!-- Custom CSS -->
     <link rel="stylesheet" href="res/css/custom.css">
-        <script type="text/javascript" src="res/js/PictureCloud.js"></script>
 
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.0.js" integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc=" crossorigin="anonymous"></script>
 
-    <title>Index</title>
+    <!-- FancyApps -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.css" />
+    <script src="https://cdn.jsdelivr.net/gh/fancyapps/fancybox@3.5.7/dist/jquery.fancybox.min.js"></script> -->
+
+    <!-- JS -->
+    <script type="text/javascript" src="res/js/pictureCloud.js"></script>
+    <title>Picture Cloud</title>
+
+    <!-- <title>PictureCloud</title> -->
   </head>
   <body>
     <?php
+      /* Model and functional includes */
       // include 'config/fileLocations.php';
       include 'util/include.php';
+
+      /* includes for html page itself (html elements) */
+
+      include 'inc/menue.php';
       include 'inc/uploadPicture.php';
       // include 'inc/addTag.php';
 
