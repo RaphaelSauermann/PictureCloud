6eb6da19b1c2a264a25eb9dd4a41d97f484d1fec
rsauermann <raphaelsauermann@yahoo.de>
2020-07-10 13:50:39 +0200

    added dynamic popup/marker generation

--- inc/map.php
+++ inc/map.php
@@ -2,4 +2,5 @@
 <div id="mapid"></div>
 <script type="text/javascript">
 createMap();
+addMarker(48.210033, 16.363449, "Foto 1");
 </script>
--- res/js/pictureCloud.js
+++ res/js/pictureCloud.js
@@ -1,11 +1,14 @@
+var map;
 function createMap() {
-  var map = L.map('mapid').setView([51.505, -0.09], 13);
+   map = L.map('mapid').setView([48.210033, 16.363449], 13);
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
+}
 
-  L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+function addMarker(lat, long, text) {
+  L.marker([lat, long]).addTo(map)
+    .bindPopup(text)
     .openPopup();
 }
