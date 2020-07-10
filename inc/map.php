f6394617765ad8b6b888f2898edde474e7e326e8
rsauermann <raphaelsauermann@yahoo.de>
2020-07-10 13:40:51 +0200

    added first map generation via leaflet

--- inc/map.php
+++ inc/map.php
@@ -1,1 +1,5 @@
 <h2>Map</h2>
+<div id="mapid"></div>
+<script type="text/javascript">
+createMap();
+</script>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 session_start();
 if (!isset($_SESSION["loginStatus"])) {
-    $_SESSION["loginStatus"] = FALSE;
+    $_SESSION["loginStatus"] = false;
 }
 if (!isset($_SESSION["uid"])) {
     $_SESSION["uid"] = -1;
@@ -38,15 +38,15 @@ if (!isset($_SESSION["isAdmin"])) {
     <!-- JS -->
     <script type="text/javascript" src="res/js/pictureCloud.js"></script>
 
-
     <!-- Lighbox -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/css/lightbox.css" rel="stylesheet" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/js/lightbox.js"></script>
-    <title>Gallery</title>
 
-    <!-- PAWLIK ADDITIONAL SOURCES - Needed for dynamic functions (Pills,Collapse) -->
-    <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>-->
-    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>-->
+    <!-- Leaflet  -->
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" />
+    <!-- Make sure you put this AFTER Leaflet's CSS -->
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
+
 
     <title>Picture Cloud</title>
 
