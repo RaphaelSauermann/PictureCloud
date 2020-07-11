c40e21f339907d0c608b66e60784b44a3d1b72fa
rsauermann <raphaelsauermann@yahoo.de>
2020-07-11 14:51:42 +0200

    fixed imports of jquery

--- index.php
+++ index.php
@@ -27,7 +27,8 @@ if (!isset($_SESSION["isAdmin"])) {
     <link rel="stylesheet" type="text/css" href="res/css/chat.css">
 
     <!-- JS, Popper.js, and jQuery -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
 
@@ -39,9 +40,6 @@ if (!isset($_SESSION["isAdmin"])) {
     <!-- JS -->
     <script type="text/javascript" src="res/js/pictureCloud.js"></script>
     <script type="text/javascript" src="res/js/chat.js"></script>
-
-    <!-- PAWLIK ADDITIONAL SOURCES - JQuery Slim build does not include ajax functions, full build is needed -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <!-- Lighbox -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/css/lightbox.css" rel="stylesheet" />
